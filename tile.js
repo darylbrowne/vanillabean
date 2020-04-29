091b3b7104e83994ede8a2e300f0fcca6d5e15f4
Daryl Browne <darylbrowne@post.harvard.edu>
2020-04-29 13:58:15 -0400

    Update tile.js

--- tile.js
+++ tile.js
@@ -1,12 +1,12 @@
 
-function slab(json) {
-    //document.write "This granite slab(json) was given..." + json;
-    throw new Error("Thrown from this granite slab()");
+function tile_engine(json) {
+    document.write "This granite slab(json) was given..." + json;
+    //throw new Error("Thrown from this granite slab()");
 }
 
 function tile(json) {
     try {
-        return slab(json);
+        return tile_engine(json);
     } catch (e) {
         throw e;
     } finally {
