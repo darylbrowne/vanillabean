e132520603b892877f2e511e8605010f3a040e2a
Daryl Browne <darylbrowne@post.harvard.edu>
2020-05-03 07:13:32 -0400

    Update person.test.js

--- test/person.test.js
+++ test/person.test.js
@@ -1,5 +1,5 @@
 // person.test.js
-const Person = require('./js/person');
+const Person = require('../js/person');
 test('Person constructs with a first and last name', () => {
   let testPerson = new Person('John', 'Doe')
   expect(testPerson).toEqual({firstName: 'John', lastName: 'Doe'});
